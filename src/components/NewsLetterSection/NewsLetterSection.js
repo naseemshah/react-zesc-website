bd6da572a14973f0e363bc1652407e42f87d6ba9
Naseem Shah <naseemshahvarikodan@gmail.com>
2020-10-16 17:43:39 +0530

    Styles for newsletter section

--- src/components/NewsLetterSection/NewsLetterSection.js
+++ src/components/NewsLetterSection/NewsLetterSection.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './Newsletter.css';
+import underlineIcon from "./underline.svg";
+import rectPat from './rect-pat.svg'
 
 function NewsletterSection() {
 
   return (
         <section id="NewsletterSection">
-           <h1>Subscribe Newsletter</h1>
-           <div>
-                <input></input>
+            <div className="news-heading-container">
+                <h1>Subscribe Newsletter</h1>
+                <img src={underlineIcon} alt=''></img>
+            </div>
+           
+           <div  className="news-input-container">
+                <input placeholder="Your Email..."></input>
                 <button>Subscribe</button>
+           </div>
+           <div class="news-pattern">
+               <img src={rectPat} alt=""></img>
            </div>
         </section>
   );
