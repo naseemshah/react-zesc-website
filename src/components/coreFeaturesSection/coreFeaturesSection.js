637b2d250edbd1f77b7e65705dd40924661dc9f5
Naseem Shah <naseemshahvarikodan@gmail.com>
2020-10-16 10:48:46 +0530

    added content in core features

--- src/components/coreFeaturesSection/coreFeaturesSection.js
+++ src/components/coreFeaturesSection/coreFeaturesSection.js
@@ -3,10 +3,49 @@ import './coreFeatures.css';
 
 
 function CoreFeaturesSection() {
+    //Icons
+    let contents = [
+        {
+            title: 'An E-commerce project with a DeFi integration',
+            icon: '/assets/img/cart.svg',
+            desc: 'ZSEC protocol is a DeFi resolution that’s on purpose to present where so many other projects have crashed. It’s invented to be a project that is a connection between De fiand E-Commerce.'
+        },
+        {
+            title: 'Staking',
+            icon: '/assets/img/staking.svg',
+            desc: 'ZSEC protocol is a decentralised finance (Defi) platform to present a resolution for Defi to entry e-commerce such as shopping staking.'
+        },
+        {
+            title: 'Yield Farming',
+            icon: '/assets/img/yield.svg',
+            desc: 'ZSEC protocol is a DeFi resolution that’s on purpose to present where so many other projects have crashed. It’s invented to be a project that is a connection between De fiand E-Commerce.'
+        },
+        {
+            title: 'Buy Back',
+            icon: '/assets/img/buyBack.svg',
+            desc: 'When the number of tokens in the shopping capital is burned. A farther quantity of tokens will be repurchased from the store. This method renewed over time and will significantly expand the value of ZSEC protocol.'
+        }
+    ]
    
   return (
         <section id="coreFeaturesSection">
-            <h1>coreFeaturesSection</h1>          
+            <div className="core-features-heading-container">
+                <h1>Core Features</h1>
+            </div>
+            <div className="core-features-contents">
+                {
+                    contents.map((item,id)=>{
+                        return (
+                            <div key={id}>
+                                <img src={item.icon} alt={item.title}></img>
+                                <h1>{item.title}</h1>
+                                <p>{item.desc}</p>
+                            </div>
+                        )
+                    })
+                }                
+            </div>
+
         </section>
   );
 }
