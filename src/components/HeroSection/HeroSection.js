c79fdad8a245460f7ac9a980be1e0cc927f96cf8
Naseem Shah <naseemshahvarikodan@gmail.com>
2020-10-16 08:36:12 +0530

    Introsuced styles for Herosection

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -8,14 +8,14 @@ import heroImage from './hero-image.svg';
 function Navbar() {
 
   return (
-        <section>
-            <img src={heroImage} alt="HeroImage"></img>
+        <section id="HeroSection">
+            <img className="hero-image" src={heroImage} alt="HeroImage"></img>
             <h1>ZSEC protocol is the world’s first E-commerce project
             combining yield farming,staking with deflationary
             components in a DEFI ecosystem.
             </h1>
             <p>Purchasing real world products, collectibles, game skins</p>
-            <div>
+            <div className="heroSectionButtonContainer">
                 <button>Uniswap</button>
                 <button>Community</button>
             </div>
