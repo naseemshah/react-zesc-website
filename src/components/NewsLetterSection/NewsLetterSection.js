d159da449bec280ab8635b396f7f0f492291b748
Naseem Shah <naseemshahvarikodan@gmail.com>
2020-10-17 10:42:59 +0530

    implemented email

--- src/components/NewsLetterSection/NewsLetterSection.js
+++ src/components/NewsLetterSection/NewsLetterSection.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Newsletter.css';
 import underlineIcon from "./underline.svg";
 import rectPat from './rect-pat.svg'
 import { useInView } from 'react-intersection-observer';
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
+import emailjs from 'emailjs-com';
 
 
 function NewsletterSection() {
+    let [isSending, setisSending] = useState(false);
+    let [message, setmessage] = useState("");
+
+    
     const { ref, inView } = useInView({
         threshold: 0,
       });
+    function sendEmail(e) {
+        setisSending(true);
+        e.preventDefault();
+    
+        emailjs.sendForm('service_wq6qi4o', 'template_r42rb9n', e.target, 'user_cpsp7zkNQPoOzcoqiGUhA')
+          .then((result) => {
+            setisSending(false);
+            setmessage("Thank you! We have sucessfully recieved you email!");
 
+          }, (error) => {
+              setisSending(false);
+            setmessage("Error! " + error.text );
+          });
+    }
   return (
       <div  ref={ref} >
           {inView && <motion.section transition={{delay: 0.5, duration: 1}} initial={{opacity:0}} animate={{opacity: 1}} id="NewsletterSection" className="lax" data-lax-preset="fadeIn"
@@ -23,10 +41,39 @@ function NewsletterSection() {
                     <img src={underlineIcon} alt=''></img>
                 </div>
                
-               <div  className="news-input-container">
-                    <input placeholder="Your Email..."></input>
-                    <button>Subscribe</button>
+               <form  className="news-input-container" onSubmit={sendEmail}>
+                    <input type="email" name="from_email" required placeholder="Your Email..."></input>
+                    <button type="submit">Subscribe</button>
+               </form>
+               <div className="formMessage">
+                   
+                       {
+                           isSending && 
+                            <AnimatePresence>
+                                <motion.p
+                                transition={{delay: 0, duration: 0.5}}
+                                exit={{ opacity: 0 }}
+                                initial={{opacity: 0}}
+                                animate={{opacity: 1}}
+                                >Sending Email...</motion.p>
+                            </AnimatePresence>                           
+                       }
+                       {
+
+                            (!isSending) && 
+                            <AnimatePresence>
+                                <motion.p
+                                transition={{delay: 0, duration: 0.5}}
+                                exit={{ opacity: 0 }}
+                                initial={{opacity: 0}}
+                                animate={{opacity: 1}}
+                            >{message}</motion.p>
+                            </AnimatePresence>
+                       }
+                   
+                   
                </div>
+
                <div className="news-pattern">
                    <img src={rectPat} alt=""></img>
                </div>
